23f5ec08cfcaff8e451437e39545ad24c05b08e8
Your GitHub Username <yuvrajsinghsoni06@gmail.com>
2026-04-13 10:09:35 +0530

    feat: add Emotion for styling and refactor Button component
    - Added @emotion/react and @emotion/styled dependencies to package.json.
    - Refactored Button component to use styled-components for styling instead of inline styles.
    - Updated button styles to improve appearance and hover effects.

--- website/my-react-app/src/button.jsx
+++ website/my-react-app/src/button.jsx
@@ -1,17 +1,27 @@
 // import styles from './button.module.css'
-
+import styled from '@emotion/styled'
+const StyleButton =styled.button`
+   color: turquoise;
+   background-color: black;
+   padding: 10px 20px;
+   border-radius: 4px;
+  
+   &:hover {
+    color: hotpink;
+  }
+`
 function Button(){
 
-    const styles ={
-       backgroundColor:"rgb(147, 147, 210)",
-       color:"white",
-       padding: "10px 20px",
-       border:"none",
-       cursor: "pointer",
-       borderRadius:"5px"
-}
+    // const styles ={
+    //    backgroundColor:"rgb(147, 147, 210)",
+    //    color:"white",
+    //    padding: "10px 20px",
+    //    border:"none",
+    //    cursor: "pointer",
+    //    borderRadius:"5px"
+// }
     
-    return(<button style={styles}>Handsome</button>);
+    return(<StyleButton>Handsome</StyleButton>);
 }
 
 export default Button;
