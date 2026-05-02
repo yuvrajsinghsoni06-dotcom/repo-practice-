40a0e4f479fc72677305bb111c640a1ac212b278
Your GitHub Username <yuvrajsinghsoni06@gmail.com>
2026-05-02 17:01:57 +0530

    feat: refactor Mycomponent to manage car details and remove unused state variables

--- website/my-react-app/src/Mycomponent.jsx
+++ website/my-react-app/src/Mycomponent.jsx
@@ -1,61 +1,30 @@
-import React ,{useState} from "react";
+import React , {useState} from 'react'
 
-function Mycomponent(){
+function Mycomponent() {
 
-    const [Name , setname] = useState("Guest");
-    const [quantity , setquantity] = useState(0);
-    const [comment , setComment] = useState(" I Realist");
-    const [payment , setPayment] = useState("");
-    const [shipped , setShipped] = useState("Delivery");
+    const [car , setCar] = useState({year : 2025 , 
+                                    make : "Ford" , 
+                                    model : "Mustang"
+    });
 
-
-    function handleEvent(e){
-        setname(e.target.value);
+    function handleYearChange(e){
+        setCar(c => ({...c,year: e.target.value}));
     }
-
-    function handlequantity(e){
-        setquantity(e.target.value);
+    function handleMakeChange(e){
+        setCar(c => ({...c,make: e.target.value}));
     }
-
-    function handlecomment(e){
-        setComment(e.target.value);
+    function handleModelChange(e){
+        setCar(c => ({...c,model: e.target.value}))
     }
+  return (
+    <div>
+        <p className="box-1">Your Favourite car is : {car.year} {car.make} {car.model}</p>
 
-    function handlePayment(e){
-        setPayment(e.target.value);
-    }
-
-    function handleShipping(e){
-        setShipped(e.target.value);
-    }
-    return(
-        <div className="box-1">
-            <input className="btn4" value={Name} onChange={handleEvent}/>
-            <p className="btn1"> Name : {Name}</p>
-            <input className="btn4" value={quantity} onChange={handlequantity}/>
-            <p className="btn2"> Quantity : {quantity}</p>
-            <input className="btn4" value={comment} onChange={handlecomment}/>
-            <p className="btn2"> Comment : {comment}</p>
-
-            <select className="btn4" value={payment} onChange={handlePayment}>
-                <option value="">Select an Option</option>
-                <option value="credit">Credit Card </option>
-                <option value="debit">Debit card</option>
-                <option value="Mastercard">Mastercard</option>
-            </select>
-            <label>
-                <input type="radio" name="shipping" value="Pick up" checked={shipped === "Pick Up"} onChange={handleShipping} />
-                pick Up
-            </label>
-            <label>
-                <input type="radio" name="shipping" value="Delivery" checked={shipped === "Delivery"} onChange={handleShipping} />
-                Delivery
-            </label>
-            <p> Shipping : {shipped}</p>
-
-        </div>
-    );
-
+        <input className="btn1" type="number" value={car.year} onChange={handleYearChange}></input><br></br>
+        <input className="btn2" type="Name" value={car.make} onChange={handleMakeChange}></input><br></br>
+        <input className="btn4" type="Model" value={car.model} onChange={handleModelChange}></input><br></br>
+    </div>
+  )
 }
 
-export default Mycomponent;
+export default Mycomponent
