b4d22f8d74429ff5c8a5f24890c5500b93b4242a
Your GitHub Username <yuvrajsinghsoni06@gmail.com>
2026-05-30 15:43:04 +0530

    practice

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-
+import Student from "./student.jsx"
 import UserList from "./Search.jsx"
+import Counter from "./counter.jsx"
 
 function App() {
-
-
-
-  return(<>
-  {/* <Hooks/> */}
-  <UserList/>
-  </>
+  return (
+    <>
+      {/* <Hooks/> */}
+      {/* <UserList/> */}
+      {/* <Student name="Yuvraj" age={30} IsStudent={true} /> */}
+      <Counter/>
+    </>
   );
 }
 
